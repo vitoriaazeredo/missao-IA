6649e89f46afa6d881d2b38aaa56e451bede0905
vitória <114578603+vitoriaazeredo@users.noreply.github.com>
2024-08-06 14:10:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
@@ -10,7 +12,7 @@ const perguntas = [
             { texto: "Médio", afirmacao: "Você tem um bom entendimento sobre o Desmatamento." },
             { texto: "Alto", afirmacao: "Você tem um ótimo conhecimento, bem aprofundado sobre o Desmatamento." }
         ]
-    }
+    },
     {
         enunciado: "Indique um fenômeno geomorfológico aumentado pelo desmatamento.",
         alternativas: [
@@ -20,10 +22,19 @@ const perguntas = [
             { texto: "Tectonismo", afirmacao: "Alternativa incorreta." }
         ]
     }
-}
+];
+{
+enunciado: "Qual o seu nível de conhecimento sobre o Desmatamento?",
+alternativas: [
+{
+texto: "Baixo",
 
 let atual = 0;
 let historiaFinal = "";
+afirmacao: "Você sabe apenas o básico sobre o Desmatamento"
+},
+{
+texto: "Médio",
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
@@ -39,18 +50,136 @@ function mostraPergunta() {
         botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativa);
     });
+afirmacao: "Você tem um bom entendimento sobre o Desmatamento. "
+},
+{
+texto: "Alto",
+
+afirmacao: "Você tem um ótimo conhecimento, bem aprofundado sobre o Desmatamento"
 }
+]
+},
+{
+enunciado: "1 -Indique um fenômeno geomorfológico que é diretamente aumentado pelo desmatamento.
+",
+alternativas: [
+{
+texto: "a) Sedimentação",
 
 function respostaSelecionada(opcaoSelecionada) {
     historiaFinal += opcaoSelecionada.afirmacao + " ";
     atual++;
     mostraPergunta();
+afirmacao: " 1- a) Alternativa incorreta." 
+},
+{
+texto: "b) Vulcanismo",
+
+afirmacao: "Alternativa incorreta."
+},
+{
+texto: "c) Erosão",
+afirmacao: "Alternativa correta!
+Erosão é o fenômeno geomorfológico diretamente aumentado pelo desmatamento. O desmatamento remove a cobertura vegetal que protege o solo, tornando-o mais vulnerável à ação da chuva e do vento, o que intensifica os processos erosivos.
+"
+},
+{
+texto: "d) Tectonismo.",
+afirmacao: "Alternativa incorreta. "
 }
 
 function mostraResultado() {
     caixaPerguntas.textContent = "Obrigado por completar o quiz!";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.innerHTML = "";
-}
+]
+},
+{
+enunciado: "2 - Assinale a alternativa que NÃO apresenta uma causa do desmatamento.
+",
+alternativas: [
+{
+texto: "a) Exploração de madeira.
+",
+afirmacao: "Alternativa incorreta. "
+},
+{
+texto: "b) Produção de carvão.",
 
+afirmacao: "2- b) Alternativa incorreta"
+
+},
+{
+texto: "c) Expansão da urbanização.",
+afirmacao: "Alternativa incorreta. "
+},
+{
+texto: "d) Conservação dos rios.
+",
+afirmacao: "Alternativa correta!Conservação dos rios não apresenta uma causa do desmatamento."
+}
+]
+},
+{
+enunciado: "3 - Qual impacto provocado pelo desmatamento está fortemente ligado à fauna e à flora de uma região?
+",
+alternativas: [
+{
+texto: "a) Diminuição da biodiversidade.",
+afirmacao: "Alternativa correta!
+Diminuição da biodiversidade é um impacto provocado pelo desmatamento que está fortemente ligado à fauna e à flora de uma região. A destruição de habitats naturais leva à perda de espécies vegetais e animais, reduzindo a diversidade biológica."
+},
+{
+texto: "b) Elevação do nível dos oceanos.
+ "3-b)Alternativa incorreta."
+
+mostraPergunta();
+},
+{
+texto: "c) Contaminação do nível freático.",
+afirmacao: "Alternativa incorreta."
+
+},
+
+{
+texto: "d) Expansão das nuvens de poeira.",
+afirmacao: "Alternativa incorreta.”
+}
+]
+}
+  
+];
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+function mostraPergunta() {
+if (atual >= perguntas.length) {
+mostraResultado();
+return;
+}
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+caixaAlternativas.textContent = "";
+mostraAlternativas();
+}
+function mostraAlternativas(){
+for(const alternativa of perguntaAtual.alternativas) {
+const botaoAlternativas = document.createElement("button");
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click", () =>
+respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas);
+}
+}
+function respostaSelecionada(opcaoSelecionada) {
+const afirmacoes = opcaoSelecionada.afirmacao;
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
+}
+function mostraResultado() {
+caixaPerguntas.textContent = "Obrigado por completar o quiz!";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+}
 mostraPergunta();
